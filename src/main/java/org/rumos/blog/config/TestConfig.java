978191593093067de3259aa67a2d958d3e308cf6
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-04-08 21:30:28 +0100

    TestConfig class creation

---
+++ src/main/java/org/rumos/blog/config/TestConfig.java
@@ -0,0 +1,26 @@
+package org.rumos.blog.config;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import org.rumos.blog.model.Person;
+import org.rumos.blog.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class TestConfig implements CommandLineRunner{
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Person person1 = new Person(null, "Tiago Ferreira", LocalDate.now());
+        Person person2 = new Person(null, "Rute Andrade", LocalDate.now());
+
+        personRepository.saveAll(Arrays.asList(person1,person2));
+    }
+
+
+}
