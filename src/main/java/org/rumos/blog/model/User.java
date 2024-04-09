4cb6da077ff839e094a711b76cddd9e837e22412
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-04-09 23:56:49 +0100

    User class creation

---
+++ src/main/java/org/rumos/blog/model/User.java
@@ -0,0 +1,72 @@
+package org.rumos.blog.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String password, String role) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+
+    
+}
