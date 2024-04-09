564ef2308f403e9406ac56c0d2b1e0e5eee061ba
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-04-10 00:01:01 +0100

    UserService class creation

---
+++ src/main/java/org/rumos/blog/services/UserService.java
@@ -0,0 +1,30 @@
+package org.rumos.blog.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.rumos.blog.model.User;
+import org.rumos.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> person = userRepository.findById(id);
+        return person.get();
+    }
+
+}
