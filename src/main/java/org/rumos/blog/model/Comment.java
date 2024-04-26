125c25460918368f306675e722a7a5d4f0bce754
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-04-26 21:04:23 +0100

    Comment class creation

---
+++ src/main/java/org/rumos/blog/model/Comment.java
@@ -0,0 +1,72 @@
+package org.rumos.blog.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comments")
+public class Comment implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(length = 1000)
+    private String text;
+    private LocalDate dateOfPublication;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, String title, String text, LocalDate dateOfPublication) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.dateOfPublication = dateOfPublication;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDate getDateOfPublication() {
+        return dateOfPublication;
+    }
+
+    public void setDateOfPublication(LocalDate dateOfPublication) {
+        this.dateOfPublication = dateOfPublication;
+    }    
+
+}
