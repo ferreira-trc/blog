b279bdc18c24c7240589c299501a3f1a46e68dcb
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-05-19 03:12:40 +0100

    feat: Post operation on Comments

--- src/main/java/org/rumos/blog/controllers/CommentController.java
+++ src/main/java/org/rumos/blog/controllers/CommentController.java
@@ -1,5 +1,6 @@
 package org.rumos.blog.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.rumos.blog.model.Comment;
@@ -8,19 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/comment")
 public class CommentController {
-
-    private CommentService commentService;
+    
+   
+    private CommentService commentService;    
 
     @Autowired
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
-    }
+    }    
 
     @GetMapping    
     public ResponseEntity<List<Comment>> findAll() {
@@ -32,5 +38,13 @@ public class CommentController {
     public ResponseEntity<Comment> getMethodName(@PathVariable Long id) {
         Comment comment = commentService.findById(id);
         return ResponseEntity.ok().body(comment);
+    }
+
+    @PostMapping
+    public ResponseEntity<Comment> post(@RequestParam Long postId, @RequestBody Comment comment) {
+        comment = commentService.add(postId,comment);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(comment.getId()).toUri();        
+        return ResponseEntity.created(uri).body(comment);
     }
 }
--- src/main/java/org/rumos/blog/services/CommentService.java
+++ src/main/java/org/rumos/blog/services/CommentService.java
@@ -4,19 +4,22 @@ import java.util.List;
 import java.util.Optional;
 
 import org.rumos.blog.model.Comment;
+import org.rumos.blog.model.Post;
 import org.rumos.blog.repositories.CommentRepository;
+import org.rumos.blog.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CommentService {
 
+    @Autowired
     private CommentRepository commentRepository;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository) {
-        this.commentRepository = commentRepository;
-    }
+    private PostRepository postRepository;
 
     public List<Comment> findAll() {
         return commentRepository.findAll();
@@ -25,5 +28,14 @@ public class CommentService {
     public Comment findById(Long id) {
         Optional<Comment> comment = commentRepository.findById(id);
         return comment.get();
-    }    
+    }   
+    
+    public Comment add(Long postId,Comment comment) { 
+        Optional<Post> commentedPost = postRepository.findById(postId);
+        if (commentedPost.isPresent()) {
+            comment.setPost(commentedPost.get());
+            return commentRepository.save(comment);
+        }     
+        throw new EntityNotFoundException("Post not found");
+    }
 }
