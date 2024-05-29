53d7994e35a509b8cc2d177d317a22acc8a2cc13
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-05-29 17:39:03 +0100

    feat: creation of the method findByUserName on UserRepository

--- src/main/java/org/rumos/blog/repositories/UserRepository.java
+++ src/main/java/org/rumos/blog/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.rumos.blog.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long>{
+
+    public User findByUserName(String userName);
     
 }
 
