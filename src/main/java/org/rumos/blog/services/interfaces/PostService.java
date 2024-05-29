2f11f3cd6a01f5a1c6c31ca8a91463ec036b45c1
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-05-29 01:51:58 +0100

    feat: creation of the class PostGetDTO

--- src/main/java/org/rumos/blog/services/interfaces/PostService.java
+++ src/main/java/org/rumos/blog/services/interfaces/PostService.java
@@ -2,12 +2,13 @@ package org.rumos.blog.services.interfaces;
 
 import java.util.List;
 
+import org.rumos.blog.model.dtos.post.PostGetDTO;
 import org.rumos.blog.model.entities.Post;
 
 public interface PostService {
-    public List<Post> findAll();
-    public List<Post> findAllByCronOrder();
-    public Post findById(Long id);
+    public List<PostGetDTO> findAll();
+    public List<PostGetDTO> findAllByCronOrder();
+    public PostGetDTO findById(Long id);
     public Post add(Post post);
     public Post update(Long postId, Post postUpdated);
     public void delete(Long id);
