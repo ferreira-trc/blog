b1976e3eda7f938cc2bd09676598c091312c26fa
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-05-30 14:18:19 +0100

    feat: creation of the class UserMapDTOImp

---
+++ src/main/java/org/rumos/blog/model/dtos/maps/implementations/UserMapDTOImp.java
@@ -0,0 +1,44 @@
+package org.rumos.blog.model.dtos.maps.implementations;
+
+import org.rumos.blog.model.dtos.maps.interfaces.UserMapDTO;
+import org.rumos.blog.model.dtos.user.UserDTOToAdd;
+import org.rumos.blog.model.dtos.user.UserDTOToShow;
+import org.rumos.blog.model.dtos.user.UserDTOToUpdate;
+import org.rumos.blog.model.entities.User;
+import org.rumos.blog.model.enums.Role;
+
+public class UserMapDTOImp implements UserMapDTO{
+
+    @Override
+    public User convertToClass(UserDTOToAdd entityDTO) {
+        User entity = new User();
+        entity.setEmail(entityDTO.email());
+        entity.setUserName(entityDTO.userName());
+        entity.setPassword(entityDTO.password());
+        entity.setRole(entityDTO.role());
+
+        return entity;
+    }
+
+    @Override
+    public User convertToClass(UserDTOToUpdate entityDTO, User entity) {
+        entity.setEmail(entityDTO.email());
+        entity.setUserName(entityDTO.userName());
+        entity.setPassword(entityDTO.password());
+        entity.setRole(entityDTO.role());
+
+        return entity;
+    }
+
+    @Override
+    public UserDTOToShow convertToDTO(User entity) {
+        Long id = entity.getId();
+        String email = entity.getEmail();
+        String userName = entity.getUserName();
+        Role role = entity.getRole();
+        UserDTOToShow entityDTO = new UserDTOToShow(id, email, userName, role);
+
+        return entityDTO;
+    }
+    
+}
