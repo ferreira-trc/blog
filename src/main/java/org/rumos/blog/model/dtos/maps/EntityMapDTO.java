ea635b708720768235e056d77aa1bfff289e140e
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-05-30 14:19:41 +0100

    refactor: Creation of the package implementations

--- src/main/java/org/rumos/blog/model/dtos/maps/EntityMapDTO.java
+++
@@ -1,8 +0,0 @@
-package org.rumos.blog.model.dtos.maps;
-
-public interface EntityMapDTO<Entity, DTOToShow, DTOToAdd, DTOToUpdate> {
-
-    public Entity convertToClass(DTOToAdd entityDTO);
-    public Entity convertToClass(DTOToUpdate entityDTO, Entity entity);
-    public DTOToShow convertToDTO(Entity entity);
-}
