002459dcf78a57072f50fa747929f656a116c40d
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-03 00:21:02 +0100

    feat: creation of the methods findByPostId and findByPostIdOrderByCreatedAtDesc

--- src/main/java/org/rumos/blog/repositories/CommentRepository.java
+++ src/main/java/org/rumos/blog/repositories/CommentRepository.java
@@ -1,8 +1,12 @@
 package org.rumos.blog.repositories;
 
+import java.util.List;
+
 import org.rumos.blog.model.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>{
      
+    public List<Comment> findByPostId(Long postId);
+    public List<Comment> findByPostIdOrderByCreatedAtDesc(Long postId);
 }
