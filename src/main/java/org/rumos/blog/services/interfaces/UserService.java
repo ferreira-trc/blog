86367ef60fbfb7e871c04384b2e36f7af8a5c670
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-21 18:44:50 +0100

    refactor: chaged the method add to register

--- src/main/java/org/rumos/blog/services/interfaces/UserService.java
+++ src/main/java/org/rumos/blog/services/interfaces/UserService.java
@@ -2,14 +2,14 @@ package org.rumos.blog.services.interfaces;
 
 import java.util.List;
 
-import org.rumos.blog.model.dtos.entities.user.UserDTOToAdd;
+import org.rumos.blog.model.dtos.entities.user.UserDTOToRegister;
 import org.rumos.blog.model.dtos.entities.user.UserDTOToShow;
 import org.rumos.blog.model.dtos.entities.user.UserDTOToUpdate;
 
 public interface UserService {
     public List<UserDTOToShow> findAll();
     public UserDTOToShow findById(Long id);
-    public UserDTOToShow add(UserDTOToAdd userDTO);
+    public UserDTOToShow register(UserDTOToRegister userDTO);
     public UserDTOToShow update(Long postId, UserDTOToUpdate userUpdated);
     public UserDTOToShow delete(Long id);
 }
