634e27a402de9867cd0b1acd1367305a89a8bf76
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-25 23:56:18 +0100

    feat: creation of the method findByEmail

--- src/main/java/org/rumos/blog/repositories/PersonRepository.java
+++ src/main/java/org/rumos/blog/repositories/PersonRepository.java
@@ -1,8 +1,12 @@
 package org.rumos.blog.repositories;
 
+import java.util.Optional;
+
 import org.rumos.blog.model.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person,Long>{
+
+    public Optional<Person> findByEmail(String email);   
 
 }
