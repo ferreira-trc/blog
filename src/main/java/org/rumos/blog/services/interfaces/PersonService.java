53760142a5e4504d039504c2c8e53d98554b022a
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-26 01:11:48 +0100

    feat: remake PersonService and implementation

--- src/main/java/org/rumos/blog/services/interfaces/PersonService.java
+++ src/main/java/org/rumos/blog/services/interfaces/PersonService.java
@@ -2,10 +2,11 @@ package org.rumos.blog.services.interfaces;
 
 import java.util.List;
 
-import org.rumos.blog.model.entities.Person;
+import org.rumos.blog.model.dtos.entities.person.PersonDTOToShow;
+import org.rumos.blog.model.dtos.entities.person.PersonDTOToUpdate;
 
 public interface PersonService {
-    public List<Person> findAll();
-    public Person findById(Long id);
-    public Person add(Person person);
+    public List<PersonDTOToShow> findAll();
+    public PersonDTOToShow findById(Long id);
+    public PersonDTOToShow update(Long id, PersonDTOToUpdate personUpdated);
 }
