8aba27e3338a497ccea5af0228b7fd53557f1a18
Tiago Ferreira <tiagoferreira977@gmail.com>
2024-06-30 11:09:10 +0100

    feat: creation findAllByAuthor method

--- src/main/java/org/rumos/blog/repositories/PostRepository.java
+++ src/main/java/org/rumos/blog/repositories/PostRepository.java
@@ -3,9 +3,11 @@ package org.rumos.blog.repositories;
 import java.util.List;
 
 import org.rumos.blog.model.entities.Post;
+import org.rumos.blog.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
 
     public List<Post> findAllByOrderByCreatedAtDesc();
+    public List<Post> findAllByAuthor(User author);
 }
